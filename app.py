6441774d449e4b373dfd75bd32fa547d6a65b18e
= <=>
2026-05-18 06:59:22 -0500

    ads

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'dtntakumi13@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Ghostsoldier12*' 
+
+app.config['MAIL_PASSWORD'] = 'yaeo djrh bvzl ytns' 
+
 app.config['MAIL_DEFAULT_SENDER'] = 'dtntakumi13@gmail.com'
 
 # Inicialización de extensiones
